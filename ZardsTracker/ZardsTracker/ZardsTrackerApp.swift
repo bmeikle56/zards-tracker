eceb0f9964c12163ec858dfd5cab5c83f4d41ebb
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2026-01-04 17:35:50 -0500

    Initial commit

---
+++ ZardsTracker/ZardsTracker/ZardsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZardsTrackerApp.swift
+//  ZardsTracker
+//
+//  Created by Braeden Meikle on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ZardsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZardsTracker/ZardsTrackerTests/ZardsTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZardsTrackerTests.swift
+//  ZardsTrackerTests
+//
+//  Created by Braeden Meikle on 1/4/26.
+//
+
+import Testing
+@testable import ZardsTracker
+
+struct ZardsTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
